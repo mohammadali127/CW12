966fac8e658d71a89fefde564a96c8e3cf692b1f
mohammadali127 <m.a.soltanhosseini@gmail.com>
2023-06-22 16:08:33 +0330

    q4 commmit

---
+++ query4.sql
@@ -0,0 +1,7 @@
+SELECT category.name, COUNT(film.film_id) AS "Total Films"
+FROM film JOIN film_category
+ON film.film_id = film_category.film_id
+JOIN category
+ON film_category.category_id = category.category_id
+GROUP BY category.name
+HAVING COUNT(film.film_id) BETWEEN 60 AND 68
