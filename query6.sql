f3368d6d8a2e7659e4accedb6d72ed7e49a17724
mohammadali127 <m.a.soltanhosseini@gmail.com>
2023-06-22 16:46:21 +0330

    q6 commit

---
+++ query6.sql
@@ -0,0 +1,8 @@
+SELECT customer.first_name, customer.last_name, (rental.return_date - rental.rental_date) as rental_duration
+, film.title
+FROM customer JOIN rental
+ON customer.customer_id = rental.customer_id
+JOIN inventory
+ON rental.inventory_id = inventory.inventory_id
+join film 
+on inventory.film_id = film.film_id
